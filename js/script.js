5ba1dc250476fbeade6ae481998336be95121d04
davidebortoloni <davidebortoloni@gmail.com>
2021-07-05 15:26:22 +0200

    Script multipli di 3 e 5 [esercizio finito]

---
+++ js/script.js
@@ -0,0 +1,37 @@
+firstColumnID = document.getElementById("first-column");
+secondColumnID = document.getElementById("second-column");
+thirdColumnID = document.getElementById("third-column");
+fourthColumnID = document.getElementById("fourth-column");
+var firstMessage = "";
+var secondMessage = "";
+var thirdMessage = "";
+var fourthMessage = "";
+var partialMessage;
+
+for (var i = 1; i <= 100; i++) {
+    partialMessage = "<div>" + i + "</div>";
+    if (i % 3 == 0) {
+        partialMessage = "<div>Fizz</div>";
+    }
+    if (i % 5 == 0) {
+        partialMessage = "<div>Buzz</div>";
+    }
+    if (i % 3 == 0 && i % 5 == 0) {
+        partialMessage = "<div>FizzBuzz</div>";
+    }
+
+    if (i <= 25) {
+        firstMessage += partialMessage;
+    } else if (i <= 50) {
+        secondMessage += partialMessage;
+    } else if (i <= 75) {
+        thirdMessage += partialMessage;
+    } else {
+        fourthMessage += partialMessage;
+    }
+}
+
+firstColumnID.innerHTML = firstMessage;
+secondColumnID.innerHTML = secondMessage;
+thirdColumnID.innerHTML = thirdMessage;
+fourthColumnID.innerHTML = fourthMessage;
